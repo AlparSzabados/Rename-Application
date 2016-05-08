6fb498aa22f6cefa2ca3b11aa2a60148050ef2f8
AlparSzabados <c3sp3nar@yahoo.com>
2016-05-08 20:50:19 +0300

    Static field no longer needed. reworked the method for easier testing. Now it uses and returns `List` instead of `Files`.

--- src/main/groovy/rename/RenameProgram.groovy
+++ src/main/groovy/rename/RenameProgram.groovy
@@ -1,24 +1,23 @@
 package rename
 
 class RenameProgram {
-    static int newCounter
-    static String fromExtension
-    static String toExtension
-    static boolean shouldDeleteSuffix
 
-    static newName(File file) {
-        println "Processing input: ${file}"
-        def matcher = (file.name =~ /(?i)^(\d+)([a-z]?)\.${fromExtension}$/)[0]
-        def (_, String counter, String part) = matcher
+    static newName(List fileName, int newCounter, String fromExtension, String toExtension, Boolean shouldDeleteSuffix) {
+        List newFileName = []
+        for (String file in fileName) {
+            def matcher = (file =~ /(?i)^(\d+)([a-z]?)\.${fromExtension}$/)[0]
+            def (_, String counter, String part) = matcher
 
-        if (!part || part.equalsIgnoreCase('a'))
-            newCounter++
+            if (!part || part.equalsIgnoreCase('a'))
+                newCounter++
 
-        def newCounterPad = "${newCounter}".padLeft(counter.size(), '0')
+            def newCounterPad = "${newCounter}".padLeft(counter.size(), '0')
 
-        if (shouldDeleteSuffix)
-            part = ''
+            if (shouldDeleteSuffix)
+                part = ''
 
-        "${file.parent}/${newCounterPad}${part}.${toExtension}" as File
+            newFileName += "${newCounterPad}${part}.${toExtension}"
+        }
+        newFileName
     }
 }
