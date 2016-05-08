a801e4e8bb6ac444b4a826360df339ce30e3f8d9
AlparSzabados <c3sp3nar@yahoo.com>
2016-05-08 20:51:37 +0300

    Added `FILE_NAMES_AS_LIST` to convert the file names into list.

--- src/main/groovy/rename/RootFolder.groovy
+++ src/main/groovy/rename/RootFolder.groovy
@@ -3,4 +3,13 @@ package rename
 class RootFolder {
     static File ROOT_FOLDER = 'C:/Rename' as File
     static File[] SORTED_FILES = ROOT_FOLDER.listFiles().sort()
+
+    //TODO find a prettier solution
+    static List FILE_NAMES_AS_LIST() {
+        def list = []
+        for (file in SORTED_FILES) {
+            list += file.name.toString()
+        }
+        list
+    }
 }
