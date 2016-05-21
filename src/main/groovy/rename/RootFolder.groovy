8372c3c7b8788d12a7569e53ec6663ca4e1d285a
AlparSzabados <c3sp3nar@yahoo.com>
2016-05-21 10:48:48 +0300

    modified method name to camelCase for consistency.

--- src/main/groovy/rename/RootFolder.groovy
+++ src/main/groovy/rename/RootFolder.groovy
@@ -5,7 +5,7 @@ class RootFolder {
     static File[] SORTED_FILES = ROOT_FOLDER.listFiles().sort()
 
     //TODO find a prettier solution
-    static List FILE_NAMES_AS_LIST() {
+    static List fileNameAsList() {
         def list = []
         for (file in SORTED_FILES) {
             list += file.name.toString()
