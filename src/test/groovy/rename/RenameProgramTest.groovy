7f58d969de0bc012a3505d11186b32f9cbfe8b42
AlparSzabados <szabadosalpar@gmail.com>
2016-09-26 13:40:06 +0300

    Added new test.

--- src/test/groovy/rename/RenameProgramTest.groovy
+++ src/test/groovy/rename/RenameProgramTest.groovy
@@ -13,6 +13,7 @@ class RenameProgramTest extends Specification {
                 ['01a.pag', '01b.pag', '02.pag']                       | 1       | 'pag'         | 'pag'       | false              || ['02a.pag', '02b.pag', '03.pag']
                 ['01.pag', '02.pag', '04.pag', '05a.pag', '05b.pag']   | 0       | 'pag'         | 'pag'       | false              || ['01.pag', '02.pag', '03.pag', '04a.pag', '04b.pag']
                 ['01a.pag', '01b.pag', '02a.pag', '03a.pag']           | 0       | 'pag'         | 'mus'       | true               || ['01.mus', '01.mus', '02.mus', '03.mus']
+                ['01a.pag', '02a.pag', '03a.pag']                      | 0       | 'pag'         | 'mus'       | true               || ['01.mus', '02.mus', '03.mus']
     }
     /*@formatter:on*/
 }
